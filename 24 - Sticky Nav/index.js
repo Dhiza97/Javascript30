03868ce6db95e05595aa254e0f0b105528245af1
Joseph Garba <josephgarba97@gmail.com>
2024-01-31 19:45:54 +0100

    Text edit
    Text edits

--- 24 - Sticky Nav/index.js
+++ 24 - Sticky Nav/index.js
@@ -1,24 +1,26 @@
-// Select the nav element
+// Select the element with the ID 'main' and store it in the variable 'nav'
 const nav = document.querySelector('#main')
 
-// Get the distance of the top of the nav from the top of the document
-const topOfNav = nav.Top
+// Get the offsetTop of the 'nav' element and store it in the variable 'topOfNav'
+const topOfNav = nav.offsetTop
 
-// Function will be called when the window is scrolled
+// Function to handle fixing the navigation based on scroll position
 function fixNav() {
-  // If the current scroll position is greater than or equal to the top of the nav
-  if(window.scrollY >= topOfNav) {
-    // Add padding to the top of the body equal to the height of the nav
+  // Check if the vertical scroll position (window.scrollY) is greater than or equal to 'topOfNav'
+  if (window.scrollY >= topOfNav) {
+    // If true, set the body padding to the height of the navigation bar
     document.body.style.paddingTop = nav.offsetHeight + 'px'
+
     // Add the 'fixed-nav' class to the body
     document.body.classList.add('fixed-nav')
   } else {
-    // Otherwise, remove the padding from the top of the body
+    // If false, reset the body padding to 0
     document.body.style.paddingTop = 0
+
     // Remove the 'fixed-nav' class from the body
     document.body.classList.remove('fixed-nav')
   }
 }
 
-// Add an event listener to the window that will call fixNav when the window is scrolled
+// Add an event listener to the window, calling the 'fixNav' function when scrolling occurs
 window.addEventListener('scroll', fixNav)
